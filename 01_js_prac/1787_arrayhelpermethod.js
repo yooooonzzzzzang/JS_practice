c2b6ec1ba4bd2844ee58c84c686fbf6624d0f161
yooooonzzzzzang <cherishvalue@naver.com>
2022-10-20 22:48:24 +0900

    docs: Add 1791,1787

---
+++ 01_js_prac/1787_arrayhelpermethod.js
@@ -0,0 +1,4 @@
+const nums = [1, 2, 3, 4]
+const new_nums = nums.map((num) => num ** 3)
+
+console.log(new_nums)
--- 01_js_prac/1791_가위바위보.js
+++ 01_js_prac/1791_가위바위보.js
@@ -2,8 +2,31 @@ const p1 = ['rock', 'paper', 'scissors', 'scissors', 'rock', 'rock', 'paper', 'p
 const p2 = ['paper', 'paper', 'rock', 'scissors', 'paper', 'scissors', 'scissors', 'rock', 'rock', 'rock']
 
 const playGame = (p1_choice, p2_choice) => {
-
+    const n = p1_choice.length
+    for (let i = 0; i < n; i++) {
+        if (p1_choice[i] === 'rock' && p2_choice[i] === 'paper') {
+            console.log(2)
+        } else if (p1_choice[i] === 'rock' && p2_choice[i] === 'rock') {
+            console.log(0)
+        } else if (p1_choice[i] === 'rock' && p2_choice[i] === 'scissors') {
+            console.log(1)
+        } else if (p1_choice[i] === 'scissors' && p2_choice[i] === 'paper') {
+            console.log(1)
+        } else if (p1_choice[i] === 'scissors' && p2_choice[i] === 'scissors') {
+            console.log(0)
+        } else if (p1_choice[i] === 'scissors' && p2_choice[i] === 'rock') {
+            console.log(2)
+        } else if (p1_choice[i] === 'paper' && p2_choice[i] === 'scissors') {
+            console.log(2)
+        } else if (p1_choice[i] === 'paper' && p2_choice[i] === 'paper') {
+            console.log(0)
+        } else if (p1_choice[i] === 'paper' && p2_choice[i] === 'rock') {
+            console.log(1)
+        }
+    }
 }
+
+playGame(p1, p2)
 
 // 2
 // 0
